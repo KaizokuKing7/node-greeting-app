028cba021870d6778d24218195c4f9dc20f9c2f6
Stanley Pierre-Canel <thousandsunnygo@outlook.com>
2019-01-30 21:21:57 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const greetings = require('./greeting.js');
+const farewell = require('./farewell.js');
+
+greetings.greetingEnglish();
+greetings.greetingJapanese();
+greetings.greetingSpanish();
+greetings.greetingIcelandic();
+farewell.farewellEnglish();
+farewell.farewellJapanese();
